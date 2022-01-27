1daca877c755a9b088afee323187f3035cb1feb3
Satya Dillikar <satya.dillikar@gmail.com>
2022-01-27 10:24:54 -0800

    first commit

---
+++ test.java
@@ -0,0 +1,11 @@
+//import system;
+
+public class test {
+    public static void main(String[] args) {
+        int x;
+        System.out.println("hello world");
+        x = 5;
+        x = x+ 1;
+        System.out.println(x);
+    }
+}
